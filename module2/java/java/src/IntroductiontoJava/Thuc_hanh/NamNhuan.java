ddf6a47c8a6d450390d2c179dd2ad47d54bdd7f7
vannguyen20697 <110239577+vannguyen20697@users.noreply.github.com>
2022-10-13 20:12:12 +0700

    Add files via upload

---
+++ module2/java/java/src/IntroductiontoJava/Thuc_hanh/NamNhuan.java
@@ -0,0 +1,24 @@
+package IntroductiontoJava.Thuc_hanh;
+
+import java.util.Scanner;
+
+public class NamNhuan {
+    public static void main(String[] args) {
+        Scanner scanner =  new Scanner(System.in);
+        System.out.println(" Nhap nam vao");
+        int year = scanner.nextInt();
+        if (year % 4 == 0){
+            if (year % 100 == 0){
+                if(year % 400 == 0){
+                    System.out.println("Nam nhuan: " + year);
+                } else {
+                    System.out.println("Nam khong nhuan: " + year);
+                }
+            } else {
+                System.out.println("Nam nhuan: " + year);
+            }
+        }else {
+            System.out.println("Nam khong nhuan: " + year);
+        }
+    }
+}
