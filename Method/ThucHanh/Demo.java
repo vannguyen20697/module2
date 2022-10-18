9a3203232a535b03cb88dfef7b15ec0c13eb34ce
vannguyen20697 <110239577+vannguyen20697@users.noreply.github.com>
2022-10-18 20:25:58 +0700

    Add files via upload

---
+++ Method/ThucHanh/Demo.java
@@ -0,0 +1,46 @@
+package Method.ThucHanh;
+
+public class Demo {
+    private String name;
+    private String email;
+    private String address;
+
+    public Demo(String name, String email, String address) {
+        this.name = name;
+        this.email = email;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Demo{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
--- Method/ThucHanh/Rectangle.java
+++ Method/ThucHanh/Rectangle.java
@@ -18,19 +18,24 @@ public class Rectangle {
 
     //Định nghĩa các phương thức getArea(), getPerimeter(), display()
     public double getArea() {
+
         return this.width * this.height;
     }
 
     public double getPerimeter() {
+
         return (this.width + this.height) * 2;
     }
 
     public String display() {
-        return "Rectangle{" + "width=" + width + ", height=" + height + "}";
+
+        return
+                "Rectangle{" + "width=" + width + ", height=" + height + "}";
     }
 }
+
 //Tạo class Main chứa phương thức main để thực thi chương trìnH
- class Main{ // sao khong phai public
+class Main { // sao khong phai public
 
     public static void main(String[] args) {
         //KHAI BAO BIEN NHAP GIA TRI
@@ -42,9 +47,9 @@ public class Rectangle {
         //Khởi tạo một đối tượng thuộc lớp Rectangle với phương thức khởi tạo
         // chứa 2 tham số width, height
         Rectangle rectangle = new Rectangle(width, height);
-        System.out.println("Your Rectangle \n"+ rectangle.display());
-        System.out.println("Perimeter of the Rectangle: "+ rectangle.getPerimeter());
-        System.out.println("Area of the Rectangle: "+ rectangle.getArea());
+        System.out.println("Your Rectangle \n" + rectangle.display());
+        System.out.println("Perimeter of the Rectangle: " + rectangle.getPerimeter());
+        System.out.println("Area of the Rectangle: " + rectangle.getArea());
     }
 
 }
